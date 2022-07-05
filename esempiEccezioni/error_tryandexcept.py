397fcffb6d15cf083d1df17effd5a6ac7501a6e0
Giux2 <giuseppegiulio.roberti@gmail.com>
2022-07-05 20:25:07 +0200

    esempi gestione eccezioni

---
+++ esempiEccezioni/error_tryandexcept.py
@@ -0,0 +1,13 @@
+def linux_interaction():
+    assert ('linux' in sys.platform), "Function can only run on Linux systems."
+    print('Doing something.')
+
+try:
+    linux_interaction()
+    with open('file.log') as file:
+        read_data = file.read()
+except FileNotFoundError as fnf_error:
+    print(fnf_error)
+except AssertionError as error:
+    print(error)
+    print('Linux linux_interaction() function was not executed')
